21ed3cbad9a1f0fd096d89a06fb5a407eeb3c294
Kristina <tina.vasylenko@gmail.com>
2023-06-19 15:50:07 +0300

    Add Task model
    Added the Task model to the project with fields and relationships:
    
    - Name: Specifies the name of the task.
    - Description: Provides a detailed description of the task.
    - Created at: Stores the timestamp of when the task was created.
    - Deadline: Specifies the deadline for completing the task.
    - Is completed: Indicates whether the task is completed or not.
    - Priority: Represents the priority level of the task.
    - Task type: Defines the type or category of the task.
    - Assignees: Associates the task with one or more workers assigned to it.
    
    These fields will allow effective task management and organization within the project.

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from it_company_task_manager import settings
+
 
 class Position(models.Model):
     name = models.CharField(max_length=63)
@@ -30,3 +32,36 @@ class Worker(AbstractUser):
     def __str__(self):
         return f"{self.last_name} {self.first_name}" \
                f"({self.position})"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=False, blank=False)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=6,
+        choices=(
+            ("Urgent", "Urgent"),
+            ("High", "High"),
+            ("Normal", "Normal"),
+            ("Low", "Low"),
+        ),
+        default="Normal",
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="assignees"
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
