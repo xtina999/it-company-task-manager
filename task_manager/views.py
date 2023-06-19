6661643cf27b6add84ea049aec08e437b642ea2b
Kristina <tina.vasylenko@gmail.com>
2023-06-19 20:14:05 +0300

    Configure URLs and create index view
    - Added urls.py in task_manager app
    - Configured project-level urls.py
    - Set DIRS in settings for templates
    - Created index view to render homepage

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Worker
+
+
+def index(request):
+    num_workers = Worker.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
