04864dd9176d82bd8dc3165b1bc2aa1848a7eead
Kristina <tina.vasylenko@gmail.com>
2023-07-04 11:46:40 +0300

    Updated settings & requirements.txt

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
