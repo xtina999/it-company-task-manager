e7e6902ff86cff804f78172808de9b9e41012926
Kristina <tina.vasylenko@gmail.com>
2023-07-13 14:13:24 +0300

    Add it-company-task-manager-btlb.onrender.com to ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "it-company-task-manager-btlb.onrender.com"
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
